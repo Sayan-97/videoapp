6cee83c682b3aea75f4a3063f6255b320be2a6fc
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2022-10-20 01:11:26 +0530

    Setup of React and Firebase

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import App from './App'
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import {BrowserRouter as Router} from 'react-router-dom';
+import {theme} from './theme';
+
+ReactDOM.render(
+  <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <Router>
+      <App />
+    </Router>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
